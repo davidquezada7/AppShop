d15cea32f202e38501d2041d8cc6305abcb1caca
David Quezada work <davidquezada93@gmail.com>
2017-11-13 17:54:22 -0600

    Categories table and model created

--- database/migrations/2017_10_24_220146_create_categories_table.php
+++ database/migrations/2017_10_24_220146_create_categories_table.php
@@ -15,6 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
